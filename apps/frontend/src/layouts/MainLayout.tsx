0e0fda7d1a13bce47ecffe00740eb17f76770fda
Mikko Nurmi <nurmikko@gmail.com>
2025-03-19 00:57:58 +0200

    First version for the footer

--- apps/frontend/src/layouts/MainLayout.tsx
+++ apps/frontend/src/layouts/MainLayout.tsx
@@ -1,16 +1,18 @@
 import { Box, Container } from '@mui/material'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
   return (
     <>
       <Header />
-      <Box sx={{ pt: 10, pb: 4 }}>
-        <Container maxWidth="lg">
+      <Box sx={{ pt: 10, pb: 4, minHeight: 'calc(100vh - 200px)' }}>
+        <Container maxWidth="md">
           <Outlet />
         </Container>
       </Box>
+      <Footer />
     </>
   )
 }
