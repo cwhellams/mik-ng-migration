acf9fd538a158e3a7538ee6f393f4bd6ea9f284e
Chris Whellams <chris.whellams@gmail.com>
2025-12-02 18:52:38 +0000

    Add data initialization scripts (#331)
    Co-authored-by: chris whellams <chris.whellams@op.fi>

---
+++ sql/schema/migration/V430__SetAutoRenewAnnualMembership.sql
@@ -0,0 +1,2 @@
+update member.register
+set auto_renew_annual_membership = true
---
+++ sql/schema/migration/V440__InsertInitialAircraftPricing.sql
@@ -0,0 +1,21 @@
+insert into accts.aircraft_pricing (
+        registration,
+        valid_from,
+        price_per_min,
+        created_by,
+        updated_by
+    )
+values (
+        'OH-STL',
+        '2025-12-01',
+        3.6,
+        'k1mnimda',
+        'k1mnimda'
+    ),
+    (
+        'OH-IHQ',
+        '2025-12-01',
+        2.45,
+        'k1mnimda',
+        'k1mnimda'
+    );
